6c3401f7d7cb46d506a152ab772a0e4506fa4a9e
alpakaxaxa <34175210+alpakaxaxa@users.noreply.github.com>
2020-07-12 21:18:58 +0200

    Add files via upload

--- SwitzerlandCoronaUpdate.sh
+++ SwitzerlandCoronaUpdate.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
-#curl -s 'https://raw.githubusercontent.com/daenuprobst/covid19-cases-switzerland/master/summary.json' | jq -r '.changes' > changes.json
+curl -s 'https://raw.githubusercontent.com/daenuprobst/covid19-cases-switzerland/master/summary.json' | jq -r '.changes' > changes.json
 CASES=$(jq -r '.cases' changes.json)
 DEATHS=$(jq -r '.fatalities' changes.json)
 TODAY=$(date +"%d. %B, %Y")
 SOURCE_CODE=$(cat ./SourceCode.txt)
-echo "<h2>Corona Update $TODAY</h2><p>Heeei Besar, es git i dä Schwiiz $CASES neui Fäll und $DEATHS Toti</p><p>Liebe Gruess<br>Raspberry Pi vom Stephan</p><p>Source Code:</p><p>$SOURCE_CODE</p>" | mail -s "$(echo -e "Corona Schweiz Update\nContent-Type: text/html")" stephan.dint.mueller@gmail.com
+echo "<h2>Corona Update $TODAY</h2><p>Heeei Besar, es git i dä Schwiiz $CASES neui Fäll und $DEATHS Toti</p><p>Liebe Gruess<br>Raspberry Pi vom Stephan</p><p>Source Code: <a href=\"https://github.com/alpakaxaxa/Corona_Update_Besar/blob/master/SwitzerlandCoronaUpdate.sh\">Github-Link</a></p>" | mail -s "$(echo -e "Corona Schweiz Update\nContent-Type: text/html")" stephan.dint.mueller@gmail.com
+echo "Mail sent successfully!"
